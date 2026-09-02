5bcab2006b076fd0c4ffd2c846bbc1b89f5c57ff
Hugo Bonilla <a20221576@pucp.edu.pe>
2026-09-01 20:04:18 -0500

    Segundo commit

--- app/src/main/java/com/example/lab1_20230420/MainActivity.java
+++ app/src/main/java/com/example/lab1_20230420/MainActivity.java
@@ -1,24 +1,51 @@
 package com.example.lab1_20230420;
 
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvResultado;
+
+    private final ActivityResultLauncher<Intent> activityResultLauncher =
+            registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+                if (result.getResultCode() == Activity.RESULT_OK && result.getData() != null) {
+                    // Lectura del extra devuelto[cite: 4, 5]
+                    String datoDevuelto = result.getData().getStringExtra("llave_retorno");
+                    tvResultado.setText("Retornado: " + datoDevuelto);
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnEnviar = findViewById(R.id.idbutton);
+        btnEnviar.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            activityResultLauncher.launch(intent); // Inicia la actividad en la pila (Back Stack)[cite: 5]
         });
+
+
+        btnPopup.setOnClickListener(this::mostrarPopupMenu);
+
+
     }
 }
